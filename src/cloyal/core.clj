332f59806c22847a9b447e18b9f76dda8d32f5d7
ujihisa <ujihisa@gmail.com>
2012-03-08 22:10:23 -0800

    first commit

---
+++ src/cloyal/core.clj
@@ -0,0 +1,1 @@
+(ns cloyal.core)
---
+++ test/cloyal/test/core.clj
@@ -0,0 +1,6 @@
+(ns cloyal.test.core
+  (:use [cloyal.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
