9db31783fae6dc645a5393ba166e89c99ed0158a
ujihisa <ujihisa@gmail.com>
2012-03-08 23:39:50 -0800

    groups data structure

--- src/cloyal/core.clj
+++ src/cloyal/core.clj
@@ -1,1 +1,48 @@
-(ns cloyal.core)
+(ns cloyal.core
+  (:require [swank.swank])
+  (:require [clojure.string :as s])
+  (:import [org.bukkit Bukkit Material])
+  (:import [org.bukkit.entity Animals Arrow Blaze Boat CaveSpider Chicken
+            ComplexEntityPart ComplexLivingEntity Cow Creature Creeper Egg
+            EnderCrystal EnderDragon EnderDragonPart Enderman EnderPearl
+            EnderSignal ExperienceOrb Explosive FallingSand Fireball Fish
+            Flying Ghast Giant HumanEntity Item LightningStrike LivingEntity
+            MagmaCube Minecart Monster MushroomCow NPC Painting Pig PigZombie
+            Player PoweredMinecart Projectile Sheep Silverfish Skeleton Slime
+            SmallFireball Snowball Snowman Spider Squid StorageMinecart
+            ThrownPotion TNTPrimed Vehicle Villager WaterMob Weather Wolf
+            Zombie])
+  (:import [org.bukkit.event.entity CreatureSpawnEvent CreeperPowerEvent
+            EntityChangeBlockEvent
+            EntityCombustByBlockEvent EntityCombustByEntityEvent
+            EntityCombustEvent EntityCreatePortalEvent EntityDamageByBlockEvent
+            EntityDamageByEntityEvent
+            EntityDamageEvent EntityDeathEvent EntityEvent EntityExplodeEvent
+            EntityDamageEvent$DamageCause
+            EntityInteractEvent EntityPortalEnterEvent
+            EntityRegainHealthEvent EntityShootBowEvent EntityTameEvent
+            EntityTargetEvent ExplosionPrimeEvent
+            FoodLevelChangeEvent ItemDespawnEvent ItemSpawnEvent PigZapEvent
+            PlayerDeathEvent PotionSplashEvent ProjectileHitEvent
+            SheepDyeWoolEvent SheepRegrowWoolEvent SlimeSplitEvent])
+  (:require clj-http.client))
+
+(defn lingr [x]
+  nil)
+
+(defn broadcast [& strs]
+  (.broadcastMessage (Bukkit/getServer) (apply str strs)))
+
+(def groups (atom {}))
+
+(defn broadcast-groups []
+  (letfn [(unwords [xs]
+            (apply str (interpose " " xs)))]
+    (doseq [[gname players] @groups]
+      (broadcast "[" gname "]: " (unwords (map (memfn getDisplayName) players))))))
+
+(defonce swank* nil)
+(defn on-enable [plugin]
+  (when (nil? swank*)
+    (def swank* (swank.swank/start-repl 4007)))
+  (lingr "cloyal plugin running..."))
