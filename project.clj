c271530eeb840ac06f197862e7f996a834b4ccac
ujihisa <ujihisa@gmail.com>
2012-03-10 15:47:07 -0800

    cloyal depend on cloft. no need for duplicate functions like broadcast

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "FIXME: write description"
   :dependencies [[clj-http "0.3.1"]
                  [swank-clojure/swank-clojure "1.3.3"]
-                 [org.clojure/clojure "1.3.0"]]
+                 [org.clojure/clojure "1.3.0"]
+                 [cloft "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[org.bukkit/bukkit "1.2.3-R0"]]
   :javac-options {:destdir "classes/"}
   :java-source-path "javasrc")
