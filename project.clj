560c6f1645cee9d1111002947f7605d7376a493e
ujihisa <ujihisa@gmail.com>
2012-03-10 15:56:02 -0800

    new cloft as a library doesn't require you to have a boiletplate java code to hook events

--- project.clj
+++ project.clj
@@ -4,6 +4,4 @@
                  [swank-clojure/swank-clojure "1.3.3"]
                  [org.clojure/clojure "1.3.0"]
                  [cloft "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[org.bukkit/bukkit "1.2.3-R0"]]
-  :javac-options {:destdir "classes/"}
-  :java-source-path "javasrc")
+  :dev-dependencies [[org.bukkit/bukkit "1.2.3-R0"]])
